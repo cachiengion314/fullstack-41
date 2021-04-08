03cfaccaaeb225eab660001fd18d2437b9b7c8d6
Hoang Nam <cachiengion314@gmail.com>
2021-04-08 22:23:10 +0700

    update backend mindx-images

--- backend-mindx-images/server/routes/postRoute/postController.js
+++ backend-mindx-images/server/routes/postRoute/postController.js
@@ -36,9 +36,9 @@ const getDetailPost = async (postId) => {
     const foundPost = await PostModel.findById(postId)
         .populate(`createdBy`, `email`)
         .populate({
-            path: "comments", // populate nguoc
+            path: "comments", // => find comments value of post-schema in comment-schema return 4 comment-schema || populate nguoc
             populate: {
-                path: `createdBy` // populate multiple level
+                path: `createdBy` // => find createdBy value of comment-schema in user-schema return 1 user-schema || populate multiple level
             }
         })
 
@@ -50,7 +50,6 @@ const getDetailPost = async (postId) => {
 
 
 const getPosts = async ({ offset, limit }) => {
-    // parallel processing
     const [posts, total] = await Promise.all([
         PostModel.find({})
             .skip(offset)
@@ -58,132 +57,103 @@ const getPosts = async ({ offset, limit }) => {
             .select(`-__v`)
             .populate(
                 {
-                    path: `createdBy`,  // => tuong ung $lookup trong mongodb
-                    select: `email _id` // => tuong ung voi $project trong mongodb
+                    path: `createdBy`,  // => find createdBy value of post-schema in user-schema return 1 user-schema || tuong ung $lookup trong mongodb
+                    select: `email _id` // || tuong ung voi $project trong mongodb
                 })
         ,
         PostModel.countDocuments()
     ])
 
-
-    // const [posts, total] = await Promise.all([
-    //     PostModel.aggregate(
-    //         [
-    //             {
-    //                 $lookup:
-    //                 {
-    //                     from: "users",
-    //                     localField: "createdBy",
-    //                     foreignField: "_id",
-    //                     as: "createdByUser"
-    //                 }
-    //             },
-    //             {
-    //                 $unwind: "$createdByUser"
-    //             },
-    //             {
-    //                 $project: {
-    //                     "createdByUser.__v": 0,
-    //                     "createdByUser._id": 0,
-    //                     "createdByUser.password": 0
-    //                 }
-    //             },
-    //             { $skip: offset },
-    //             { $limit: limit },
-    //         ]),
-    //     PostModel.countDocuments()
-    // ])
-
     return [posts, total]
 }
 
-const getSpecifiedPost = async (postId) => {
+// const getSpecifiedPost = async (postId) => {
 
-    // const docs = await PostModel.aggregate(
-    //     [
-    //         {
-    //             $match:
-    //             {
-    //                 _id: ObjectId(postId)
-    //             }
-    //         },
-    //         {
-    //             $lookup:
-    //             {
-    //                 from: "users",
-    //                 localField: "createdBy",
-    //                 foreignField: "_id",
-    //                 as: "_post_createdByUser"
-    //             }
-    //         },
-    //         {
-    //             $unwind: "$_post_createdByUser"
-    //         },
-    //         // => found 1 post_obj
-    //         {
-    //             $lookup:
-    //             {
-    //                 from: "comments",
-    //                 localField: "_id",
-    //                 foreignField: "postId",
-    //                 as: "_post_comments"
-    //             }
-    //         },
-    //         { "$unwind": "$_post_comments" },
-    //         // => found 4 comments created by the same user => unwind : 4 different post_obj__post_comments
-    //         {
-    //             $lookup:
-    //             {
-    //                 from: "users",
-    //                 localField: "_post_comments.createdBy",
-    //                 foreignField: "_id",
-    //                 as: "_post_commentsByUser"
-    //             }
-    //         },
-    //         {
-    //             $unwind: "$_post_commentsByUser"
-    //         },
-    //         // => found 1 user forEach 4 post_obj__post_comments => unwind : found 4 post_obj__post_comments__post_commentsByUser
-    //         {
-    //             $project:
-    //             {
-    //                 "_id": 1,
-    //                 "description": 1,
-    //                 "title": 1,
-    //                 "imageUrl": 1,
-    //                 "createdBy": "$_post_createdByUser.email",
-    //                 "comment": {
-    //                     content: "$_post_comments.content",
-    //                     createdByUser: "$_post_commentsByUser.email"
-    //                 },
-    //             }
-    //         },
-    //         {
-    //             "$group": {
-    //                 "_id": "$_id",
-    //                 "description": {
-    //                     $first: "$description"
-    //                 },
-    //                 "title": {
-    //                     $first: "$title"
-    //                 },
-    //                 "imageUrl": {
-    //                     $first: "$imageUrl"
-    //                 },
-    //                 "createdBy": {
-    //                     $first: "$createdBy"
-    //                 },
-    //                 "comments": {
-    //                     "$push": "$comment",
-    //                 },
-    //             }
-    //         },
-    //     ]
-    // )
+// const docs = await PostModel.aggregate(
+//     [
+//         {
+//             $match:
+//             {
+//                 _id: ObjectId(postId)
+//             }
+//         },
+//         {
+//             $lookup:
+//             {
+//                 from: "users",
+//                 localField: "createdBy",
+//                 foreignField: "_id",
+//                 as: "_post_createdByUser"
+//             }
+//         },
+//         {
+//             $unwind: "$_post_createdByUser"
+//         },
+//         // => found 1 post_obj
+//         {
+//             $lookup:
+//             {
+//                 from: "comments",
+//                 localField: "_id",
+//                 foreignField: "postId",
+//                 as: "_post_comments"
+//             }
+//         },
+//         { "$unwind": "$_post_comments" },
+//         // => found 4 comments created by the same user => unwind : 4 different post_obj__post_comments
+//         {
+//             $lookup:
+//             {
+//                 from: "users",
+//                 localField: "_post_comments.createdBy",
+//                 foreignField: "_id",
+//                 as: "_post_commentsByUser"
+//             }
+//         },
+//         {
+//             $unwind: "$_post_commentsByUser"
+//         },
+//         // => found 1 user forEach 4 post_obj__post_comments => unwind : found 4 post_obj__post_comments__post_commentsByUser
+//         {
+//             $project:
+//             {
+//                 "_id": 1,
+//                 "description": 1,
+//                 "title": 1,
+//                 "imageUrl": 1,
+//                 "createdBy": "$_post_createdByUser.email",
+//                 "comment": {
+//                     content: "$_post_comments.content",
+//                     createdByUser: "$_post_commentsByUser.email"
+//                 },
+//             }
+//         },
+//         {
+//             "$group": {
+//                 "_id": "$_id",
+//                 "description": {
+//                     $first: "$description"
+//                 },
+//                 "title": {
+//                     $first: "$title"
+//                 },
+//                 "imageUrl": {
+//                     $first: "$imageUrl"
+//                 },
+//                 "createdBy": {
+//                     $first: "$createdBy"
+//                 },
+//                 "comments": {
+//                     "$push": "$comment",
+//                 },
+//             }
+//         },
+//     ]
+// )
 
-    console.log(`getSpecifiedPost`, docs)
-    return docs
-}
+// console.log(`getSpecifiedPost`, docs)
+// return docs
+// }
 
 module.exports = {
     createPost,
